a64a39a50b8b0455766086d78d7dbbf6219e8bdf
OrangeTheWeak <59427507+orange-the-weak@users.noreply.github.com>
2024-10-18 19:17:33 +0800

    Update gaiagps_subscription_rewrite.js

--- gaiagps_subscription_rewrite.js
+++ gaiagps_subscription_rewrite.js
@@ -1,21 +1,70 @@
 
 async function onResponse(context, url, request, response) {
+  // console.log(response);
 
-  response.body = {
-    "user": {
-      "id": 6741313,
-      "email": "premium@gaia.com",
-      "has_password": true
-    },
-    "code": 1000,
-    "subscription": {
-      "endDate": "2099-12-31 09:45:04",
-      "startDate": "2024-10-07 09:45:04",
+  // Update or Add new headers
+  // response.headers["Content-Type"] = "application/json";
+
+  // Update status Code
+  // response.statusCode = 500;
+
+  // Update Body
+  var body = response.body;
+//   body["new-key"] = "Proxyman";
+  body={
+  "user": {
+    "id": 999,
+    "email": "premium@gaia.com",
+    "email_opt_in": null,
+    "has_password": true,
+    "upload_logs": false,
+    "verbose_logging": false
+  },
+  "code": 1000,
+  "appcode": "gaia",
+  "dataset": "gaiagreen",
+  "subscription": {
+    "created": "2024-10-07",
+    "desc": "Gaia GPS premium subscription for one year.",
+    "appname": "gaiang",
+    "dataset": "gaiagreen",
+    "autorenewing": true,
+    "name": "Gaia GPS Premium",
+    "trial": false,
+    "identifier": "GaiaGPSPremiumApple",
+    "canceled": false,
+    "provider": "appstore",
+    "endDate": "2099-10-14 09:45:04",
+    "startDate": "2024-10-07 09:45:04",
+    "originaltransactionid": "530001909808229",
+    "transactionid": "530001909808229"
+  },
+  "other_subscriptions": [
+    {
+      "created": "2024-10-07",
+      "desc": "Gaia GPS premium subscription for one year.",
+      "appname": "gaiang",
+      "dataset": "gaiagreen",
+      "autorenewing": true,
       "name": "Gaia GPS Premium",
-      "autorenewing": true
+      "trial": false,
+      "identifier": "GaiaGPSPremiumApple",
+      "canceled": false,
+      "provider": "appstore",
+      "endDate": "2099-10-14 09:45:04",
+      "startDate": "2024-10-07 09:45:04",
+      "originaltransactionid": "530001909808229",
+      "transactionid": "530001909808229"
     }
-  };
-  
-  // Return modified response
+  ],
+  "purchases": []
+}
+
+  response.body = body;
+
+  // Or map a local file as a body
+  // response.bodyFilePath = "~/Desktop/myfile.json"
+
+  // Done
   return response;
 }
